d8520b2162d8f0bcc3ebbfc970d39f5ed399b596
dcox298 <105471792+dcox298@users.noreply.github.com>
2025-01-01 14:36:08 -0600

    Update Panel.tsx

--- src/webparts/panel/components/Panel.tsx
+++ src/webparts/panel/components/Panel.tsx
@@ -1,18 +1,34 @@
 import * as React from 'react';
 import styles from './Panel.module.scss';
 import type { IPanelProps } from './IPanelProps';
-import { PrimaryButton, Stack } from '@fluentui/react';
+import { Text, Panel, PrimaryButton, Stack } from '@fluentui/react';
+import { useState } from 'react';
 
 export default function PanelBase(props:IPanelProps):JSX.Element {
+  const [isPanelOpen, setPanelOpen ] = useState(false);
   return (
     <section className={`${styles.panel}`}>
       <Stack>
         <Stack.Item>
           <PrimaryButton 
             text='button text'
+            onClick={()=>setPanelOpen(true)}
           />
         </Stack.Item>
       </Stack>
+      <Panel
+        isLightDismiss
+        headerText="Sample panel"
+        isOpen={isPanelOpen}
+        onDismiss={()=>setPanelOpen(false)}
+        closeButtonAriaLabel="Close"
+      >
+        <Stack>
+          <Stack.Item>
+            <Text>Panel Content</Text>
+          </Stack.Item>
+        </Stack>
+      </Panel>
     </section>
   );
 }
